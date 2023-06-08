613a11f3d03124b9d2dca3ab9bc4caf00f5d515d
DanilByaliy <byaliy03@gmail.com>
2023-06-08 17:00:53 +0300

    Configure modules and movies service injection

--- server/src/sessions/sessions.module.ts
+++ server/src/sessions/sessions.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SessionsService } from './sessions.service';
 import { SessionsController } from './sessions.controller';
+import { Session } from './session.entity';
+import { MoviesModule } from 'src/movies/movies.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Session]), MoviesModule],
   providers: [SessionsService],
   controllers: [SessionsController],
 })
