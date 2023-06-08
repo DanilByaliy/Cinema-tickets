c710d6e1830bd771bb5e3d3060f9a92a8deea0e1
DanilByaliy <byaliy03@gmail.com>
2023-06-08 17:04:15 +0300

    Add sessions controller functions

--- server/src/sessions/sessions.controller.ts
+++ server/src/sessions/sessions.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { SessionsService } from './sessions.service';
+import { CreateSessionDto } from './dtos/create-session.dto';
+import { UpdateSessionDto } from './dtos/update-session.dto';
 
 @Controller('sessions')
-export class SessionsController {}
+export class SessionsController {
+  constructor(private sessionsService: SessionsService) {}
+
+  @Post()
+  createSession(@Body() body: CreateSessionDto) {
+    return this.sessionsService.create(body);
+  }
+
+  @Get('/:id')
+  findSession(@Param('id') id: string) {
+    return this.sessionsService.findOne(id);
+  }
+
+  @Get()
+  findAllSessions() {
+    return this.sessionsService.find();
+  }
+
+  @Patch('/:id')
+  updateSession(@Param('id') id: string, @Body() body: UpdateSessionDto) {
+    return this.sessionsService.update(id, body);
+  }
+
+  @Delete('/:id')
+  removeSession(@Param('id') id: string) {
+    return this.sessionsService.remove(id);
+  }
+}
