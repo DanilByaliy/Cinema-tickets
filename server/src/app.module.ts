8ba3840e3fe23ca52fb80db56ce8b6719e779801
DanilByaliy <byaliy03@gmail.com>
2023-06-09 20:21:41 +0300

    Add the tickets service, controller and module generated by Nest

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { MoviesModule } from './movies/movies.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { Movie } from './movies/movie.entity';
 import { Session } from './sessions/session.entity';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { Session } from './sessions/session.entity';
     }),
     MoviesModule,
     SessionsModule,
+    TicketsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
