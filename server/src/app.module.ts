a64d7145209d0cef4202015e8c0e03f3ecc64a40
DanilByaliy <byaliy03@gmail.com>
2023-06-09 22:12:34 +0300

    Configure modules and tickets service injection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,16 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { SessionsModule } from './sessions/sessions.module';
+import { TicketsModule } from './tickets/tickets.module';
 import { Movie } from './movies/movie.entity';
 import { Session } from './sessions/session.entity';
-import { TicketsModule } from './tickets/tickets.module';
+import { Ticket } from './tickets/ticket.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [Movie, Session],
+      entities: [Movie, Session, Ticket],
       synchronize: true,
     }),
     MoviesModule,
--- server/src/tickets/tickets.module.ts
+++ server/src/tickets/tickets.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
+import { Ticket } from './ticket.entity';
+import { SessionsModule } from 'src/sessions/sessions.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ticket]), SessionsModule],
   providers: [TicketsService],
-  controllers: [TicketsController]
+  controllers: [TicketsController],
 })
 export class TicketsModule {}
