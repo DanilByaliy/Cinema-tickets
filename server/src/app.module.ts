087ffa240c522e9cbb79d124c839978ead26b7c2
DanilByaliy <byaliy03@gmail.com>
2023-06-11 00:09:50 +0300

    Add using env variables to set up database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -16,11 +16,16 @@ import { Ticket } from './tickets/ticket.entity';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [Movie, Session, Ticket],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [Movie, Session, Ticket],
+          synchronize: true,
+        };
+      },
     }),
     MoviesModule,
     SessionsModule,
