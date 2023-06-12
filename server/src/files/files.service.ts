bef6fd62c140b6d3c8d55fcca2bcd7042a0319f4
DanilByaliy <byaliy03@gmail.com>
2023-06-12 22:55:12 +0300

    Improve file save function, apply data compression

--- server/src/files/files.service.ts
+++ server/src/files/files.service.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@nestjs/common';
 import * as uuid from 'uuid';
 import * as path from 'path';
 import { writeFile } from 'fs/promises';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class FilesService {
   async saveFile(file: Express.Multer.File) {
-    try {
-      const fileName = uuid.v4() + '.jpg';
-      const filePath = path.resolve('static', fileName);
+    const fileName = uuid.v4() + '.webp';
+    const filePath = path.resolve('static', fileName);
+    const buffer = await this.convertToWebP(file.buffer);
 
-      await writeFile(filePath, file.buffer);
+    try {
+      await writeFile(filePath, buffer);
       return fileName;
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async convertToWebP(file: Buffer): Promise<Buffer> {
+    return sharp(file).webp().toBuffer();
   }
 }
