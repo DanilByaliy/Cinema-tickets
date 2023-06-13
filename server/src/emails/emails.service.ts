2fddeb99c724be51a87c9963091d3dbe0abe43b9
DanilByaliy <byaliy03@gmail.com>
2023-06-13 23:51:21 +0300

    Add a service constructor with the creation of a transporter

--- server/src/emails/emails.service.ts
+++ server/src/emails/emails.service.ts
@@ -4,4 +4,21 @@ import * as nodemailer from 'nodemailer';
 @Injectable()
 export class EmailsService {
   private transporter: nodemailer.Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport(
+      {
+        host: '',
+        port: 587,
+        secure: false,
+        auth: {
+          user: '',
+          pass: '',
+        },
+      },
+      {
+        from: '',
+      },
+    );
+  }
 }
