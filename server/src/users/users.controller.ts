583ddd794e47cd93c0a6c5f5e5870903404470c6
DanilByaliy <byaliy03@gmail.com>
2023-06-14 23:46:53 +0300

    Apply a serialization decorator to hide the user password

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
