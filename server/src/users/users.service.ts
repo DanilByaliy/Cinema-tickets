1a8f411a221373d4149a645893c38002664610cd
DanilByaliy <byaliy03@gmail.com>
2023-06-14 23:42:21 +0300

    Add user creation endpoint

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private repo: Repository<User>,
+  ) {}
+
+  create(body: CreateUserDto) {
+    const user = this.repo.create(body);
+    return this.repo.save(user);
+  }
+}
