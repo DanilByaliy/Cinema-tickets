ba1ece29ad168cc323bd47c49536b5844451a570
DanilByaliy <byaliy03@gmail.com>
2023-06-16 22:29:42 +0300

    Add dependency injection and order creation endpoint

--- server/src/orders/orders.controller.ts
+++ server/src/orders/orders.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrderDto } from './dtos/order.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private ordersService: OrdersService) {}
+
+  @Post()
+  async makeOrder(@Body() body: OrderDto) {
+    await this.ordersService.makeOrder(body);
+  }
+}
