5248152e879185b2460e55a7c1b9f043e4d304f1
DanilByaliy <byaliy03@gmail.com>
2023-06-27 23:06:47 +0300

    feat: add websocket server connection handling

--- server/src/gateway/gateway.ts
+++ server/src/gateway/gateway.ts
@@ -1,4 +1,16 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnModuleInit } from '@nestjs/common';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class MyGateway {}
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      const { sessionId: roomId } = socket.handshake.query;
+      socket.join(roomId);
+    });
+  }
+}
