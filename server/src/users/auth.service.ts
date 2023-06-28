5e01e848928f42b252b7dc55923995eca2c01753
DanilByaliy <byaliy03@gmail.com>
2023-06-28 22:15:27 +0300

    feat: implement a sign up method

--- server/src/users/auth.service.ts
+++ server/src/users/auth.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
-  signup(email: string, password: string) {
-    //
+
+  async signup(body: CreateUserDto) {
+    const { email, password } = body;
+    const existingUser = this.usersService.findOneByEmail(email);
+    if (existingUser) throw new BadRequestException('Email in use');
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create({ email, password: result });
+    return user;
   }
 
   signin(email: string, password: string) {
