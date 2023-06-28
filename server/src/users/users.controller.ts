50a3656d9e0963f98f161732dd479a1e894f8996
DanilByaliy <byaliy03@gmail.com>
2023-06-28 22:55:13 +0300

    feat: connect the auth service methods to the controller

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -3,11 +3,25 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Post('/signup')
+  signup(@Body() body: CreateUserDto) {
+    return this.authService.signup(body);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body);
+  }
 
   @Post()
   createUser(@Body() body: CreateUserDto) {
