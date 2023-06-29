7c3617dd3216431b08ba9d3ba6f009bc4937aa50
DanilByaliy <byaliy03@gmail.com>
2023-06-29 23:56:16 +0300

    feat: add an endpoint for changing the user's role

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Get, Param, Post, Session } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { ChangeRoleDto } from './dtos/change-role.dto';
+import { Role } from 'src/decorators/roles-auth.decorator';
+import { RoleGuard } from 'src/guards/role.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -40,5 +43,12 @@ export class UsersController {
   @Get('/:email')
   findUser(@Param('email') email: string) {
     return this.usersService.findOneByEmail(email);
+  }
+
+  @Role('ADMIN')
+  @UseGuards(RoleGuard)
+  @Post('/role')
+  changeRole(@Body() body: ChangeRoleDto) {
+    return this.usersService.changeRole(body);
   }
 }
