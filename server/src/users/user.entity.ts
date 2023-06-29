4cecca7e16acbea53925baf96955515f24f2fa7f
DanilByaliy <byaliy03@gmail.com>
2023-06-29 23:42:08 +0300

    feat: add the role property to the user and the DTO to change the role

--- server/src/users/user.entity.ts
+++ server/src/users/user.entity.ts
@@ -3,11 +3,14 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
   @Column()
   email: string;
 
   @Column()
   password: string;
+
+  @Column('varchar', { default: 'USER' })
+  role: 'USER' | 'ADMIN';
 }
