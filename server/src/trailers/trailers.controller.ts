37e0bdb93647e74d4ac6bdb96a504f1d53b2f0e6
DanilByaliy <byaliy03@gmail.com>
2023-08-08 23:58:58 +0300

    feat(trailers): add a video stream receiving endpoint

--- server/src/trailers/trailers.controller.ts
+++ server/src/trailers/trailers.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Res,
+  HttpStatus,
+  Header,
+} from '@nestjs/common';
+import { statSync, createReadStream } from 'fs';
+import { Headers } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('trailers')
-export class TrailersController {}
+export class TrailersController {
+  @Get(':id/stream')
+  @Header('Accept-Ranges', 'bytes')
+  @Header('Content-Type', 'video/mp4')
+  async getStreamVideo(
+    @Param('id') id: string,
+    @Headers() headers,
+    @Res() res: Response,
+  ) {
+    const videoPath = `${id}.mp4`;
+    const { size } = statSync(videoPath);
+    const videoRange = headers.range;
+    if (videoRange) {
+      const parts = videoRange.replace(/bytes=/, '').split('-');
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
+      const chunksize = end - start + 1;
+      const readStreamfile = createReadStream(videoPath, {
+        start,
+        end,
+        highWaterMark: 60,
+      });
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${size}`,
+        'Content-Length': chunksize,
+      };
+      res.writeHead(HttpStatus.PARTIAL_CONTENT, head);
+      readStreamfile.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': size,
+      };
+      res.writeHead(HttpStatus.OK, head);
+      createReadStream(videoPath).pipe(res);
+    }
+  }
+}
